aa1df20afda94ddc9a294316dc948b7f3d7fdfc6
Chaitali Patidar <99090643+chaitalipatidar2003@users.noreply.github.com>
2024-01-20 19:22:51 +0530

    Update index.js

--- mern server/index.js
+++ mern server/index.js
@@ -4,7 +4,11 @@ const port = process.env.PORT|| 5000
 const cors=require('cors')
 
 //middleware
-app.use(cors())
+app.use(cors({
+  origin:[https://book-store-gold-three.vercel.app/],
+          methods:["POST", "GET"],
+          credentials: true
+            ))
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
